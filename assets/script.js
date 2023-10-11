f78b98a5e0b50372daf14b00f97a4a07228fafca
NarvinBlas@gmail.com <NarvinBlas@gmail.com>
2023-10-11 00:51:04 -0400

    created array for to be included in scheduler

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ function updateTime() {
 $('#currentDay').text(date.format('dddd, MMM D, YYYY'));
 
 let now = dayjs().format("pp");
-  for (let i = 0, i < blockArr.length; i++) {
+  for (let i = 0; i < blockArr.length; i++) {
     blockArr[i].removeClass("future past present");
 
     if (now > blockArr[i].data("hour")) {
@@ -18,3 +18,33 @@ let now = dayjs().format("pp");
     }
   }
 }
+
+setInterval(updateTIme, 1000);
+
+
+var saveBtn = $(".saveHolder");
+var blockContainer = $(".container");
+var block9am = $("#hour-9");
+var block10am = $("#hour-10");
+var block11am = $("#hour-11");
+var block12pm = $("#hour-12");
+var block1pm = $("#hour-1");
+var block2pm = $("#hour-2");
+var block3pm = $("#hour-3");
+var block4pm = $("#hour-4");
+var block5pm = $("#hour-5");
+
+var blockArr = [
+  block9am, 
+  block10am,
+  block11am,
+  block12pm,
+  block1pm,
+  block2pm,
+  block3pm,
+  block4pm,
+  block5pm,
+];
+
+
+updateTime()
